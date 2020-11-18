6ad5ec778d1c07dd7d11e7cebdf90a12ca9ca001
fisher <1079736477@qq.com>
2020-11-18 19:06:18 +0800

    Abstract the codec

--- codec/codec.go
+++ codec/codec.go
@@ -1,1 +1,22 @@
 package codec
+
+import (
+    "io"
+)
+
+type NewCodec func(closer io.ReadWriteCloser) Codec
+
+type Codec interface {
+    Encoder
+    Decoder
+    Close() error
+    String() string
+}
+
+type Encoder interface {
+    Encode(interface{}) error
+}
+
+type Decoder interface {
+    Decode(interface{}) error
+}
