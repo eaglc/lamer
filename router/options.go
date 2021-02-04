8f511fdb3943b136cb3ee7b29b784e12df1106db
yukunlei <yukunlei@51talk.com>
2021-02-04 17:20:24 +0800

    router.options

--- router/options.go
+++ router/options.go
@@ -1,7 +1,64 @@
 package router
 
-type Options struct {
+import "context"
 
+type Options struct {
+	RouteBefore RouteBefore
+	Before      []ServeBefore
+	After       []ServeAfter
+	Finalizer   []ServeFinalizer
+	PoolSize    int
+	ChanSize    int
+	ChanCount   int
+	Context     context.Context
 }
 
 type Option func(*Options)
+
+func Route(b RouteBefore) Option {
+	return func(o *Options) {
+		o.RouteBefore = b
+	}
+}
+
+func Before(b ...ServeBefore) Option {
+	return func(o *Options) {
+		o.Before = b
+	}
+}
+
+func After(a ...ServeAfter) Option {
+	return func(o *Options) {
+		o.After = a
+	}
+}
+
+func Final(f ...ServeFinalizer) Option {
+	return func(o *Options) {
+		o.Finalizer = f
+	}
+}
+
+func PoolSize(s int) Option {
+	return func(o *Options) {
+		o.PoolSize = s
+	}
+}
+
+func ChanSize(s int) Option {
+	return func(o *Options) {
+		o.ChanSize = s
+	}
+}
+
+func ChanCount(c int) Option {
+	return func(o *Options) {
+		o.ChanCount = c
+	}
+}
+
+func Context(ctx context.Context) Option {
+	return func(o *Options) {
+		o.Context = ctx
+	}
+}
--- router/router.go
+++ router/router.go
@@ -1,10 +1,25 @@
 package router
 
-import "github.com/eaglc/lamer/handler"
+import (
+    "context"
+    "github.com/eaglc/lamer/handler"
+)
+
+// RouteBefore is executed on the request before the router.Serve
+// One of the uses is to develop a distribution strategy
+type RouteBefore func(ctx context.Context, req interface{}) context.Context
+
+// ServeBefore is executed on the request before the handler
+type ServeBefore func(ctx context.Context, req interface{}) context.Context
+type ServeAfter func(ctx context.Context, replay interface{}) context.Context
+type ServeFinalizer func(ctx context.Context, reply interface{}) error
 
 type Router interface {
     handler.Handler
-    Handle(pattern interface{}, handler handler.Func)
+    Init(opts ...Option)
+    Handle(pattern interface{}, handler handler.Func, opts ...Option)
+    Start() error
+    Stop() error
     String() string
 }
 
