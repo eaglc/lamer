3aa98fd24589d66d2716725de160cb0d69e1cf78
yukunlei <yukunlei@51talk.com>
2021-03-17 19:02:46 +0800

    config interface

--- config/config.go
+++ config/config.go
@@ -1,1 +1,45 @@
 package config
+
+type EventType int
+
+const (
+	CREATE EventType = iota
+	UPDATE
+	DELETE
+)
+
+type Event struct {
+	Type     EventType
+	Key      string
+	OldValue string
+	NewValue string
+}
+
+type Center interface {
+	Init(...Option)
+	GetConfig(conf string) Config
+}
+
+type Config interface {
+	GetProperty(key string, defValue string) string
+	GetContent() string
+	AddWatcher(watcher Watcher)
+}
+
+type Watcher interface {
+	Next() (*Event, error)
+	Stop()
+}
+
+func (et EventType) String() string {
+	switch et {
+	case CREATE:
+		return "create"
+	case UPDATE:
+		return "update"
+	case DELETE:
+		return "delete"
+	default:
+		return "unknown"
+	}
+}
--- config/options.go
+++ config/options.go
@@ -1,1 +1,13 @@
 package config
+
+type Options struct {
+	App string
+}
+
+type Option func(*Options)
+
+func App(app string) Option {
+	return func(o *Options) {
+		o.App = app
+	}
+}
